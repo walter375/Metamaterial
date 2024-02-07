b5732865682f741e19ce7695fa25ca01fe11c18b
kero811 <k.m.rodko@gmail.com>
2024-02-07 13:54:58 +0100

    dU updated, error wen optimize is run

--- code/rigidbody_2D.py
+++ code/rigidbody_2D.py
@@ -11,7 +11,7 @@ _/\_    0_1/ \3_5
        0,0
             
 """
-#todo rewrite U for reshaped positions
+
 #todo fix dU
 #todo test!
 nb_bodies = 6
@@ -39,18 +39,17 @@ def U_2D(positions_ic, beam_lengths_n, k_n):
     return U
 
 def dU(positions_ic, beam_lengths_n, k_n):
-    dU = np.zeros(len(positions_ic))
-    for i in range(len(positions_ic)):
-        # first beam
-        if i == 0:
-            dU[i] = -k_n[i]*(np.linalg.norm(positions_ic[i+1] - positions_ic[i]) - beam_lengths_n[i])
-        # last beam
-        elif i == (len(positions_ic)-1):
-            dU[i] = k_n[i-1]*(np.linalg.norm(positions_ic[i] - positions_ic[i-1]) - beam_lengths_n[i-1])
-        # inbetween beams
-        else:
-            dU[i] = k_n[i-1]*(np.linalg.norm(positions_ic[i] - positions_ic[i-1]) - beam_lengths_n[i-1]) \
-                    - k_n[i]*(np.linalg.norm(positions_ic[i+1] - positions_ic[i]) - beam_lengths_n[i])
+    dU = np.zeros(nb_bodies)
+    # loop over all beams
+    for m in range(nb_bodies):
+        # find all beams connected to hinge i
+        for k in range(nb_bodies):
+            if (i_n[k] == m or j_n[k] == m):
+                index_j = j_n[k]
+                index_i = i_n[k]
+                dU[m] += k_n[k] * (np.linalg.norm(positions_ic[index_j] - positions_ic[index_i]) - beam_lengths_n[k])
+            else:
+                continue
     return dU
 
 def getBeamLength_2D(positions_ic):
@@ -64,33 +63,34 @@ def getBeamLength_2D(positions_ic):
         beam_lengths_n[m] = np.linalg.norm(positions_ic[index_j] - positions_ic[index_i])
     return beam_lengths_n
 
-def objective(positions_2i, beam_lengths_n, k_n):
-    positions_ic = positions_2i.reshape(nb_hinges, 2)
+def objective(positions_flat_2i, beam_lengths_n, k_n):
+    positions_ic = positions_flat_2i.reshape(nb_hinges, 2)
     #print(positions_ic)
     return U_2D(positions_ic, beam_lengths_n, k_n)
 
 # first position needs to stay zero
-def con1(positions_2i):
+def con1(positions_flat_2i):
     # print("con1: ", positions_2i[0:2], start)
-    return start - positions_2i[0:2]
+    return start - positions_flat_2i[0:2]
 
 # last position needs to stay the same, cannot move
-def con2(positions_2i):
+def con2(positions_flat_2i):
     # print("\ncon2: ",positions_2i[-2:], end)
-    return positions_2i[-2:] - end
+    return positions_flat_2i[-2:] - end
 
 if __name__ == "__main__":
     beam_lengths_n = getBeamLength_2D(positions_initial_ic)
     print("obj: ", objective(positions_flat, beam_lengths_n, k_n))
+    print("dU: ", dU(positions_final_ic, beam_lengths_n, k_n))
     cons = [{'type': 'eq', 'fun': con1},
             {'type': 'eq', 'fun': con2}]
-    res = scipy.optimize.minimize(objective, x0=np.zeros(nb_hinges*2), args=(beam_lengths_n, k_n), constraints=cons ) #,jac=dU)
+    res = scipy.optimize.minimize(objective, x0=np.zeros(nb_hinges*2), args=(beam_lengths_n, k_n), constraints=cons) #,jac=dU)
     print("msg: ", res.message)
     print("x: ", res.x.reshape(nb_hinges, 2))
     print("fun: ", res.fun)
-
-    points= res.x.reshape(nb_hinges,2)
-    print(points[:,0], points[:,1])
-    plt.plot(positions_initial_ic[:,0], positions_initial_ic[:,1], 'o')
-    plt.plot(points[:,0], points[:,1], 'x')
-    plt.show()
+    #
+    # points= res.x.reshape(nb_hinges,2)
+    # print(points[:,0], points[:,1])
+    # plt.plot(positions_initial_ic[:,0], positions_initial_ic[:,1], 'o')
+    # plt.plot(points[:,0], points[:,1], 'x')
+    # plt.show()
