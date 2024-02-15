700f4b89403ce1e3b14dced80030deb51f5a9742
kero811 <k.m.rodko@gmail.com>
2024-02-15 13:27:48 +0100

    2D test works

--- test/test_dU_2D.py
+++ test/test_dU_2D.py
@@ -30,36 +30,47 @@ import numpy as np
 import math
 from code import rigidbody_2D as rb
 def test_dU_2D():
+    epsilon = 0.001
     positions_initial_ic = np.array([[0, 1], [1, 1], [2, 2], [3, 1], [2, 0], [4, 1]])  # shape=(nb_hinges, 2)
-    positions_final_ic = np.array([[0, 1], [1, 1], [2, 2], [3, 1], [2, 0], [4, 1.0001]])
+    positions_pulled_ic = np.array([[0, 1], [1, 1], [2, 2], [3, 1], [2, 0], [4,0]])
+    positions_epsilon_ic = np.array([[0, 1], [1, 1], [2, 2], [3, 1], [2, 0], [4,0+epsilon]])
+    positions_epsilon_half_ic = np.array([[0, 1], [1, 1], [2, 2], [3, 1], [2, 0], [4,0+epsilon/2]])
     i_n = np.array([0, 1, 2, 1, 4, 3])
     j_n = np.array([1, 2, 3, 4, 3, 5])
 
+    # positions_initial_ic = np.array([[0.,0.], [0.,1.], [0.,2.],[0.,3.]])
+    # positions_pulled_ic = np.array([[0.,0.], [0.,1.], [0.,2.],[1.,3.]])
+    # positions_epsilon_ic = np.array([[0.,0.],[0.,1.], [0.,2.],[1+ epsilon,3.]])
+    # positions_epsilon_half_ic = np.array([[0., 0.], [0., 1.], [0., 2.], [1. + epsilon/2 , 3. ]])
+    # i_n = np.array([0,1,2])
+    # j_n = np.array([1,2,3])
+
     nb_bodies = len(i_n)
     nb_hinges = len(positions_initial_ic)
-    positions_flat = positions_final_ic.reshape(nb_hinges * 2)
     k_n = np.ones(nb_bodies)
-    start = positions_initial_ic[0]
-    end = positions_final_ic[-1]
 
     beam_lengths_n = rb.getBeamLength_2D(positions_initial_ic, i_n, j_n)
 
-    u_inital = rb.U_2D(positions_initial_ic, beam_lengths_n, k_n, i_n, j_n)
-    u_final = rb.U_2D(positions_final_ic, beam_lengths_n, k_n, i_n, j_n)
+    u_initial = rb.U_2D(positions_initial_ic, beam_lengths_n, k_n, i_n, j_n)
+    u_pulled = rb.U_2D(positions_pulled_ic, beam_lengths_n, k_n, i_n, j_n)
+    u_epsilon = rb.U_2D(positions_epsilon_ic, beam_lengths_n, k_n, i_n, j_n)
 
-    du_inital = rb.dU_2D(positions_initial_ic, beam_lengths_n, k_n, i_n, j_n)
-    print("\ndu_pulled\n")
-    du_pulled = rb.dU_2D(positions_final_ic, beam_lengths_n, k_n, i_n, j_n)
+    du_initial = rb.dU_2D(positions_initial_ic, beam_lengths_n, k_n, i_n, j_n)
+    du_pulled = rb.dU_2D(positions_pulled_ic, beam_lengths_n, k_n, i_n, j_n)
+    du_epsilon = rb.dU_2D(positions_epsilon_ic, beam_lengths_n, k_n, i_n, j_n)
+    du_epsilon_half = rb.dU_2D(positions_epsilon_half_ic, beam_lengths_n, k_n, i_n, j_n)
 
-    print("\nu_inital: ", u_inital, "\nu_final: ", u_final, "\ndu_inital: ", du_inital, "\ndu_pulled: ", du_pulled)
-    epsilon = np.linalg.norm(positions_final_ic[-1]-positions_initial_ic[-1]) - np.linalg.norm(positions_initial_ic[-1]-positions_initial_ic[-1])
-    test_numerical = (u_final-u_inital) / epsilon
+    print("\nu_inital: ", u_initial, "\nu_pulled: ", u_pulled, "\nu_epsilon: ", u_epsilon, "\ndu_initial: ", du_initial, "\ndu_pulled: ", du_pulled, "\ndu_epsilon: ", du_epsilon, "\ndu_epsilon2: ", du_epsilon_half)
+
+    test_numerical = (u_epsilon-u_pulled)/epsilon
+
     # print(np.linalg.norm(positions_final_ic[-1]-positions_initial_ic[-1]))
     print("\ntest_num: ", test_numerical)
-
-    test_analytical = np.sum(du_inital[-1] + du_pulled[-1])    # print("\ntest_analytical: ", test_analytical)
+    #diff = (du_pulled[-1] - du_inital[-1])
+    #print(diff)
+    test_analytical = du_epsilon_half[-1:,1]
     print("\nanalytical: ", test_analytical)
 
     # print("dy_analytical:", dy_analytical.shape)
     # print("dy_numerical:", dy_numerical.shape)
-    #np.testing.assert_allclose(test_analytical, test_numerical, atol=1e-15)
+    np.testing.assert_allclose(test_analytical, test_numerical, rtol=1e-6, atol=1e-5)
